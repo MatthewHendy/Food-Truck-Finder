95eb35090deb305bd2c77a32ebc9e774a59916f3
Matt Hendrickson <matthewjames@utexas.edu>
2019-05-15 14:13:29 -0500

    initial commit

---
+++ Food Truck Finder/ViewControllers/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  Food Truck Finder
+//
+//  Created by macbook on 5/15/19.
+//  Copyright © 2019 Matt Hendrickson. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super .viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super .viewWillAppear(animated)
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super .viewDidAppear(animated)
+        
+    }
+    
+}
+
